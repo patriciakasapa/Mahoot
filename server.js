deeba2e36292f90d12a99c6745f7a5e284f30b32
Kay <kay@MacBook-Pro.mshome.net>
2021-01-15 10:38:56 +0000

    Tahoot Frontend

--- server.js
+++ server.js
@@ -4,31 +4,6 @@ const path = require('path');
 
 const app = express();
 
-let http = require('http');
-let server = http.Server(app);
-
-let socketIO = require('socket.io');
-let io = socketIO(server);
-
-const port = process.env.PORT || 3000;
-
-io.on('connection', (socket) => {
-    console.log('User Connected!');
-
-    //join game room
-    socket.on('game-play-room', (roomName) => {
-        socket.join(roomName);
-    });
-
-    //sending data to game room
-    socket.on('game-play-data', (roomName, data) => {
-        socket.join(roomName);
-        socket.to(roomName).emit('game-play-data', data);
-        console.log(data);
-    });
-    
-});
-
 // Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/angularapp'));
 
@@ -39,8 +14,3 @@ res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-//websocket server
-server.listen(port, () => {
-    console.log(`started on port: ${port}`);
-});
