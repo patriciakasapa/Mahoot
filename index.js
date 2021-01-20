dfce4b5fd7f202689561ade0e0dd70979f6c8151
Kay <kay@MacBook-Pro.mshome.net>
2021-01-20 09:44:30 +0000

    Websocket

---
+++ index.js
@@ -0,0 +1,56 @@
+let express = require('express')
+let app = express();
+
+var cors = require('cors')
+app.use(cors());
+
+let http = require('http');
+let server = http.Server(app);
+
+
+let socketIO = require('socket.io');
+
+let io = socketIO(server, {
+    origins: ["http://localhost:4200"],
+    
+    handlePreflightRequest: (req, res) => {
+        res.writeHead(200, {
+          "Access-Control-Allow-Origin": "http://localhost:4200",
+          "Access-Control-Allow-Methods": "GET,POST",
+          "Access-Control-Allow-Headers": "my-custom-header",
+          "Access-Control-Allow-Credentials": true
+        });
+        res.end();
+      }
+    }
+    );
+
+const port = process.env.PORT || 5000;
+
+
+io.on('connection', (socket) => {
+    console.log('User Connected!');
+        
+    // socket.on('gamer-name', (gamerName) => {
+    //     io.emit('gamer-name', gamerName);
+    //         console.log(gamerName)
+    // });
+
+    //join game room
+    socket.on('game-play-room', (roomName) => {
+        socket.join(roomName);
+    });
+
+    //sending data to game room
+    socket.on('game-play-data', (roomName, data) => {
+        socket.join(roomName);
+        socket.to(roomName).emit('game-play-data', data);
+        console.log(data);
+    });
+    
+});
+
+
+server.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
