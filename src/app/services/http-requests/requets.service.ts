52beb40f6672d856f3f1d20b072da3c88a9c0c3d
patriciakasapa <patriciakasapa07@gmail.com>
2021-03-17 10:26:41 +0000

    apiURL to get from Heroku environment variable

--- src/app/services/http-requests/requets.service.ts
+++ src/app/services/http-requests/requets.service.ts
@@ -9,7 +9,8 @@ import { environment } from 'src/environments/environment';
 })
 export class RequetsService {
 
-  apiURL = environment.apiURL;
+  // apiURL = environment.apiURL;
+  apiURL = process.env.apiURL;
   
   constructor(private http: HttpClient) { }
 
